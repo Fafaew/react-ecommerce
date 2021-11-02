262c72f13cbc8c75a3b6dd3420078ca663f5f0e8
fafaew <rafael.gomest@gmail.com>
2021-11-02 13:58:25 -0300

    General: Added logic for fetch subdivisions and buttons inside the adress form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -11,6 +11,7 @@ const steps = ['Shipping address', 'Payment details'];
 const Checkout = ( { cart }) => {
   const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
+  const [shippingData, setShippingData] = useState({});
   const classes = useStyles();
 
   useEffect(() => {
@@ -25,6 +26,15 @@ const Checkout = ( { cart }) => {
     generateToken();
   }, [cart]);
 
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1 )
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1 )
+
+  const next = (data) => {
+    setShippingData(data);
+
+    nextStep()
+  }
+
   const Confirmation = () => (
     <div>
       Confirmation
@@ -32,7 +42,7 @@ const Checkout = ( { cart }) => {
   )
 
   const Form = () => activeStep === 0
-        ? <AdressForm checkoutToken={checkoutToken} />
+        ? <AdressForm checkoutToken={checkoutToken} next={next} />
         : <PaymentForm />
 
   return (
